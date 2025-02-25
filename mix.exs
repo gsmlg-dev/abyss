aa8eb6ac63e8b6a7369882b834d07a1dc4f3b450
Jonathan Gao <gsmlg.com@gmail.com>
2025-02-26 00:09:30 +0800

    fix: Fix.

--- mix.exs
+++ mix.exs
@@ -9,8 +9,12 @@ defmodule Abyss.MixProject do
       app: :abyss,
       version: @version,
       elixir: "~> 1.14.1 or ~> 1.15",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
+      name: "Abyss",
       description: "Abyss is a pure Elixir UDP server",
+      source_url: @source_url,
+      dialyzer: dialyzer(),
       aliases: aliases(),
       package: package(),
       deps: deps(),
@@ -24,8 +28,33 @@ defmodule Abyss.MixProject do
     ]
   end
 
-  defp deps do
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps() do
     [
+      {:telemetry, "~> 0.4 or ~> 1.0"},
+      {:machete, ">= 0.0.0", only: [:dev, :test]},
+      {:ex_doc, "~> 0.25", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+      plt_add_deps: :apps_direct,
+      plt_add_apps: [:public_key],
+      flags: [
+        "-Werror_handling",
+        "-Wextra_return",
+        "-Wmissing_return",
+        "-Wunknown",
+        "-Wunmatched_returns",
+        "-Wunderspecs"
+      ]
     ]
   end
 
